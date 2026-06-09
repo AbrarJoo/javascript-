ee0699ba6d69fa2ea0d518780e35c2a5e57f7b45
AbrarJoo <log42241@gmail.com>
2026-06-09 15:19:38 +0530

    new

---
+++ test.js
@@ -0,0 +1,17 @@
+// const age = "21";
+// const agetonum = +age;
+
+// console.log(agetonum);
+// console.log(typeof agetonum);
+// console.log(typeof age);
+
+const salary = "21000";
+const negativesalary = -salary;
+
+console.log(negativesalary);
+console.log(typeof negativesalary);
+console.log(typeof salary);
+
+const is_online = true;
+console.log(is_online);
+console.log(`\nusing not operator -- ${!is_online}`);
